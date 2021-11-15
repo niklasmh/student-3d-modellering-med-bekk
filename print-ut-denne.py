48c2b9af927dc6fc0dc0af5d68f2fa9fd79bee71
Niklas Molnes Hole <nikasmh@hotmail.com>
2021-11-15 23:57:46 +0100

    Update print-ut-denne.py

--- print-ut-denne.py
+++ print-ut-denne.py
@@ -1,1 +1,3 @@
-# Lim inn koden din hit og lag en PR. Du får videre instruksjoner når du lager PR-en.
+# Trykk på "Edit this file" og lim inn koden din.
+# Lag så en PR med å trykke på "Propose changes".
+# Du vil videre få mer instruksjoner om hva du må gjøre inne på PR-siden.
